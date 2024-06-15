2ece3abf69edc370235438f20415c40745942bb7
DenysShchypt <denysshchyptsovit@gmail.com>
2024-06-15 21:01:17 +0300

    logic auth

---
+++ src/common/errors/index.ts
@@ -0,0 +1,8 @@
+export const AppErrors = {
+  USER_EXISTS: 'User with this email already exists',
+  USER_NOT_FOUND: 'User with this email not found',
+  WRONG_PASSWORD: 'Password is incorrect',
+  WRONG_EMAIL: 'Email is incorrect',
+  WRONG_EMAIL_OR_PASSWORD: 'Email or password is incorrect',
+  WRONG_TOKEN: 'Token is incorrect',
+};
