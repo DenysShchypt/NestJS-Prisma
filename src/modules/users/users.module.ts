7cb16da85b5dc50a5ec9eee8c05279261dbe5cae
DenysShchypt <denysshchyptsovit@gmail.com>
2024-07-06 22:11:44 +0300

    add cache user

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaService } from '../prisma/prisma.service';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   providers: [UsersService, PrismaService],
   controllers: [UsersController],
   exports: [UsersService],
+  imports: [
+    CacheModule.register({
+      isGlobal: true,
+    }),
+  ],
 })
 export class UsersModule {}
